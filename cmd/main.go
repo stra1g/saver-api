48b67255e7fa17a8a80343cfd5989aca9f1edd5f
Luís Vitor <luis.sg.vitor@gmail.com>
2025-04-17 21:38:55 -0300

    feat: setup fx

--- cmd/main.go
+++ cmd/main.go
@@ -1,28 +1,80 @@
 package main
 
 import (
+	"context"
 	"log"
+	"net"
+	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/stra1g/saver-api/pkg/logger"
+	"go.uber.org/fx"
 )
+
+func ProvideLogger() logger.Logger {
+	isDebug := os.Getenv("DEBUG") == "true"
+	return logger.Initialize(os.Stdout, isDebug)
+}
+
+func Server(lc fx.Lifecycle, log logger.Logger) *gin.Engine {
+	router := gin.Default()
+
+	gin.SetMode(gin.DebugMode)
+
+	router.GET("/ping", func(c *gin.Context) {
+		log.Info("Ping endpoint hit", map[string]interface{}{
+			"method": c.Request.Method,
+			"path":   c.Request.URL.Path,
+		})
+
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+ 
+	addr := os.Getenv("HOST") + ":" + os.Getenv("PORT")
+	srv := &http.Server{Addr: addr, Handler: router}
+ 
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				log.Error(err, "Failed to start HTTP Server", map[string]interface{}{
+					"addr": srv.Addr,
+				})
+				return err
+			}
+			go srv.Serve(ln)
+			log.Info("Succeeded to start HTTP Server", map[string]interface{}{
+				"addr": srv.Addr,
+			})
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			srv.Shutdown(ctx)
+			log.Info("HTTP Server is stopped", map[string]interface{}{})
+			return nil
+		},
+	})
+ 
+	return router
+}
 
 func main() {
 	err := godotenv.Load()
-  if err != nil {
-    log.Fatal("Error loading .env file")
-  }
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-	gin.SetMode(gin.DebugMode)
-	
-  router := gin.Default()
-  router.GET("/ping", func(c *gin.Context) {
-    c.JSON(200, gin.H{
-      "message": "pong",
-    })
-  })
+	app := fx.New(
+		fx.Provide(
+			ProvideLogger,
+			Server,
+		),
+		fx.Invoke(func(*gin.Engine) {}),
+	)
 
-	addr := os.Getenv("HOST") + ":" + os.Getenv("PORT")
-  router.Run(addr)
+	app.Run()
 }
